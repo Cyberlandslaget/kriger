24f8f5f2fb36100ea49f9912042c0e3c2c88b073
0xLE <78134270+0xLE@users.noreply.github.com>
2024-07-12 13:38:28 +0200

    feat(cli): add broken wip deploy command impl

--- crates/kriger/src/cli/deploy.rs
+++ crates/kriger/src/cli/deploy.rs
@@ -1,7 +1,13 @@
+use bollard::image::{BuildImageOptions, BuilderVersion};
+use bollard::models::BuildInfo;
+use bollard::secret::BuildInfoAux;
+use bollard::Docker;
+use color_eyre::Result;
+use futures::StreamExt;
+use tokio::fs;
+
 use crate::cli::args;
 use crate::cli::model::ExploitManifest;
-use color_eyre::Result;
-use tokio::fs;
 
 async fn read_exploit_manifest() -> Result<ExploitManifest> {
     let raw = fs::read("exploit.toml").await?;
@@ -18,6 +24,52 @@ pub(crate) async fn main(args: args::Deploy) -> Result<()> {
             return Err(err);
         }
     };
-    println!("\u{1F680} Deploying {}", &manifest.exploit.name);
+    println!("\u{1F680} Preparing to deploy {}", &manifest.exploit.name);
+
+    let docker = Docker::connect_with_local_defaults()?;
+
+    let opt = BuildImageOptions {
+        t: "kriger-exploit:test",
+        dockerfile: "Dockerfile",
+        version: BuilderVersion::BuilderBuildKit,
+        pull: true,
+        rm: true,
+        forcerm: true,
+        session: Some("kriger-cli".to_string()),
+        ..Default::default()
+    };
+
+    // TODO: Honor dockerignore
+    let mut tar = tar::Builder::new(Vec::new());
+    tar.append_dir_all(".", ".")?;
+    tar.finish()?;
+    let tar = tar.into_inner()?;
+
+    println!("\u{1F528} Building... (ctx: {} B)", &tar.len());
+
+    // FIXME: Currently "broken": https://github.com/fussybeaver/bollard/issues/428
+    let mut stream = docker.build_image(opt, None, Some(tar.into()));
+    while let Some(res) = stream.next().await {
+        match res {
+            Ok(BuildInfo {
+                aux: Some(BuildInfoAux::BuildKit(status)),
+                ..
+            }) => {
+                for v in status.vertexes {
+                    println!("{}: {:?}", &v.name, &v.completed);
+                    if !v.error.is_empty() {
+                        println!("  -> {}", &v.error);
+                    }
+                }
+            }
+            Ok(msg) => {
+                println!("{msg:?}");
+            }
+            Err(err) => {
+                println!("{err}");
+            }
+        }
+    }
+
     Ok(())
 }
