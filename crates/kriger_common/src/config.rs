8699a881d0df30499af5f18a73856e79f162c739
0xLE <78134270+0xLE@users.noreply.github.com>
2024-07-19 21:57:30 +0200

    feat(common): add profile-dependent default nats_url

--- crates/kriger_common/src/config.rs
+++ crates/kriger_common/src/config.rs
@@ -1,9 +1,13 @@
 use clap::Args;
+#[cfg(debug_assertions)]
+const DEFAULT_NATS_URL: &str = "nats://127.0.0.1:4222";
+#[cfg(not(debug_assertions))]
+const DEFAULT_NATS_URL: &str = "nats://nats:4222";
 
 #[derive(Args, Debug)]
 #[group(skip)]
 pub struct Config {
     /// The URL to the NATS/JetStream server
-    #[arg(env, long, default_value = "nats://127.0.0.1:4222")]
+    #[arg(env, long, default_value = DEFAULT_NATS_URL)]
     pub nats_url: String,
 }
