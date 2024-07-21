59d81eb93500b60576c4627b3228563cbd381fde
Jakob <jakob.r.skaar@student.uib.no>
2024-07-21 18:01:29 +0200

    chore: fix thing

--- crates/kriger/src/cli/args.rs
+++ crates/kriger/src/cli/args.rs
@@ -5,6 +5,7 @@ use clap_derive::Parser;
 #[command(version, about)]
 pub(crate) struct Deploy {
     /// Do not immediately launch the exploit
+    #[arg(long)]
     pub(crate) no_launch: bool,
 
     /// URL for REST API
