f95164cbc0c49840686818cc32f304f3f3ebf999
0xLE <78134270+0xLE@users.noreply.github.com>
2024-07-21 18:12:21 +0200

    fix(rest): fix config

--- crates/kriger_rest/src/config.rs
+++ crates/kriger_rest/src/config.rs
@@ -4,10 +4,6 @@ use clap_derive::Parser;
 #[command(version, about)]
 #[group(skip)]
 pub struct Config {
-    /// The URL to the NATS/JetStream server
-    #[arg(env, long, default_value = "nats://127.0.0.1:4222")]
-    pub nats_url: String,
-
     /// The socket address to listen to
     #[arg(env, long, default_value = "[::]:8000")]
     pub(crate) rest_listen: String,
