a283afc66b3089c2161097e4a912c0198fbd2573
0xLE <78134270+0xLE@users.noreply.github.com>
2024-07-28 18:57:20 +0200

    docs: add nats seeding to the dev docker environment

---
+++ data/test/nats-seed.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+nats kv put config competition '{
+  "start": "2024-01-01T08:00:00Z",
+  "tick": 5,
+  "tick_start": 0,
+  "flag_validity": 5,
+  "flag_format": "[A-Z0-9]{31}=",
+  "submitter": {
+    "type": "dummy"
+  },
+  "fetcher": {
+    "type": "dummy"
+  }
+}'
+
+# TODO: Remove once the fetcher is functional
+nats kv put services U2VydmljZSAxIENoZWNrZXIgMQ '{"name": "service 1 Checker 1", "has_hint": false}'
+
+for i in `seq 0 9`
+do
+  nats kv put teams "$i" "{\"ip_address\": \"10.60.$i.1\", \"service\":{}}"
+done
