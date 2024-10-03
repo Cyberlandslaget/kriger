d53eedb98159fd254f530d95eb34a1db92d881d6
0xLE <78134270+0xLE@users.noreply.github.com>
2024-10-03 21:08:40 +0200

    feat(frontend): use relative paths in prod

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,5 +1,11 @@
-
 export const CONFIG = {
-  webSocketUrl: import.meta.env.VITE_WS_URL ?? "http://localhost:8001",
-  restUrl: import.meta.env.VITE_REST_URL ?? "http://localhost:8000",
+  webSocketUrl: new URL(
+    import.meta.env.VITE_WS_URL ?? "http://localhost:8001",
+    location.origin,
+  ),
+  restUrl: new URL(
+    import.meta.env.VITE_REST_URL ?? "http://localhost:8000",
+    location.origin,
+  ),
 };
+
